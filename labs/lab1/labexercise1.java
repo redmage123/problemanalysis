7d65d1673ae957974912e322152c301d2d7df491
Braun Brelin <bbrelin@gmail.com>
2016-07-13 19:27:01 +0300

    1.  Modified original document.  Data Log changed to Data Dictionary 2.  Created Lab1 and Lab2 directories with lab notes and sample data.

---
+++ labs/lab1/labexercise1.java
@@ -0,0 +1,17 @@
+/*
+ * Lab1 Exercise.
+ * This exercise will take an input file called
+ * ALbb.salaries.2003.csv and allow users to 
+ * query the data using the team name as the 
+ * input key. 
+ * The output will be the aggregated salaries for that
+ * team.
+ */
+
+
+public class lab1exercise {
+    public static void main(String[] args) {
+        ; // Program body goes here 
+    }
+}
+
---
+++ labs/lab2/labexercise2.java
@@ -0,0 +1,16 @@
+/*
+ * Lab2 Exercise.
+ * This exercise will take multiple input files
+ * located in data/baseballdatabank-master/core
+ * and allow users to query the data using the team name as the 
+ * input key. The output will be the aggregated salaries for that
+ * team for any given year.
+ */
+
+
+public class lab2exercise {
+  public static void main(String[] args) {
+      ; // Program body goes here
+  }
+}
+
